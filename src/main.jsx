77622f60e3e142227d17d6800414c4622ca53363
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-04-29 13:05:40 +0300

    main jsx fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './components/app/App.jsx';
+import App from './components/app/App'; // Correct the path if needed
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
