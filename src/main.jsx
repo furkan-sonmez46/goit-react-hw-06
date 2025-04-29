43fce3bd8198a8a7fa6148c7d05e975188a1b61d
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-04-29 12:22:51 +0300

    main jsx changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './components/App/App.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './components/app/App.jsx';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
